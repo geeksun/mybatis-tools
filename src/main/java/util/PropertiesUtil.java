8d98b13ce5b02fe47b2eda69d0eac391c03e4d62
jzq1999 <330896202@qq.com>
2017-04-17 09:19:12 +0800

    Init mybatis-tools project

---
+++ src/main/java/util/PropertiesUtil.java
@@ -0,0 +1,37 @@
+package util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.Properties;
+import java.util.Set;
+
+/**
+ * Created by jzq1999 on 2017/4/12.
+ */
+public class PropertiesUtil {
+
+    private static Properties prop = new Properties();
+
+    static {
+        try {
+            InputStream in = PropertiesUtil.class.getResourceAsStream("conf.properties");
+            prop.load(in);
+            Set keyValue = prop.keySet();
+            for (Iterator it = keyValue.iterator(); it.hasNext();)
+            {
+                String key = (String) it.next();
+                System.out.println("key:" + key);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getValue(String key){
+        String value = prop.getProperty(key);
+
+        return value;
+    }
+
+}
